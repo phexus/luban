e4fb651202a64ee314f9e53e784a666b7539a565
phexus <mryanhr@gmail.com>
2023-08-16 23:43:34 +0800

    allow nullable sep bean

--- src/Luban.Job.Cfg/Source/DataCreators/ExcelStreamDataCreator.cs
+++ src/Luban.Job.Cfg/Source/DataCreators/ExcelStreamDataCreator.cs
@@ -383,14 +383,14 @@ namespace Luban.Job.Cfg.DataCreators
             {
                 if (type.IsNullable)
                 {
-                    string subType = x.Read().ToString().Trim();
+                    string subType = x.DetectReadSkipNull().ToString().Trim();
                     if (subType == DefBean.BEAN_NULL_STR)
                     {
                         return null;
                     }
                     else if (subType != DefBean.BEAN_NOT_NULL_STR && subType != originBean.Name)
                     {
-                        throw new Exception($"type:'{type.Bean.FullName}' 可空标识:'{subType}' 不合法（只能为{DefBean.BEAN_NOT_NULL_STR}或{DefBean.BEAN_NULL_STR}或{originBean.Name})");
+                        // throw new Exception($"type:'{type.Bean.FullName}' 可空标识:'{subType}' 不合法（只能为{DefBean.BEAN_NOT_NULL_STR}或{DefBean.BEAN_NULL_STR}或{originBean.Name})");
                     }
                 }
                 return new DBean(type, originBean, CreateBeanFields(originBean, x));
